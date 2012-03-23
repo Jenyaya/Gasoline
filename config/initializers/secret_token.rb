cd4c511cb4dc1694f9023d3680baa8f8641583bb
Jenyaya <jenyaya@online.ua>
2012-03-23 10:33:38 +0000

    initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gasoline::Application.config.secret_token = 'ffd60d02cbc7e124e71b4eec08817972a1575465bdc3af9250abc9188c8ae90798f5ce85a6c7064a3996184e59dde3d20bba5f427dee71b28a74183965262b96'
