4960dac5824c851d70facc61740685b31f15903f
Jenyaya <jenyaya@online.ua>
2012-03-23 12:42:41 +0000

    with pages 2

--- Gemfile
+++ Gemfile
@@ -7,7 +7,8 @@ gem 'rails', '3.1.3'
 
 gem 'sqlite3'
 gem 'rspec-rails'
-#gem 'net/http'
+gem 'hpricot'
+#gem 'open-uri'
 
 
 # Gems used only for assets and not required
--- app/models/prices.rb
+++ app/models/prices.rb
@@ -5,11 +5,16 @@ class Prices
 
   def self.get_okko
     uri = URI('http://okko.ua/uk/network-of-filling-stations')
-   Net::HTTP.get(uri)
+    Net::HTTP.get(uri)
+  end
+
+  def self.get_klo
+    uri = URI('http://klo.ua/avtozapravka/')
+    Net::HTTP.get(uri)
   end
 
 end
 
-  page = Prices.get_okko
+  page = Prices.get_klo
 
-puts page.body
+puts page
